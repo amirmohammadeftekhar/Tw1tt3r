2e55eff905923be2a6fc2dbfce004021cc1af75e
amirmohammad eftekhar <amirmohammadeftekhar@gmail.com>
2021-07-29 14:19:53 +0430

    ini

---
+++ Tw1tt3r-Server/src/main/java/com/example/tw1tt3rServer/Tw1tt3rServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tw1tt3rServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tw1tt3rServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tw1tt3rServerApplication.class, args);
+    }
+
+}
---
+++ Tw1tt3r-Server/src/test/java/com/example/tw1tt3rServer/Tw1tt3rServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tw1tt3rServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tw1tt3rServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
